3043d8583b9776672efb302fe123b3da4d3bb6c5
ajim9075shaikh <142928027+ajim9075shaikh@users.noreply.github.com>
2024-07-28 13:16:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function getNumbers() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+getNumbers()
+    .then(numbers => {
+        return numbers.filter(number => number % 2 === 0);
+    })
+    .then(evenNumbers => {
+        return evenNumbers.map(number => number * 2);
+    })
+    .then(finalNumbers => {
+        document.getElementById('output').innerText = finalNumbers.join(', ');
+    });
